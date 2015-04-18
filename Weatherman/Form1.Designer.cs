be944f8b5a3a6222481c813f50d904556c155c82
mrmadsenmalmo <mail.fredrikaugust@gmail.com>
2015-04-18 14:17:56 +0200

    simple functionality for showing text

---
+++ Weatherman/Form1.Designer.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+namespace Weatherman
+{
+    partial class Weatherman
+    {
+        [DllImport("user32.dll")]
+        private static extern int HideCaret(IntPtr hwnd);
+
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.PowerButton = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.Dialogue = new System.Windows.Forms.Label();
+            this.TextTimer = new System.Windows.Forms.Timer(this.components);
+            this.UserInput = new System.Windows.Forms.TextBox();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.CursorTimer = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // PowerButton
+            // 
+            this.PowerButton.Location = new System.Drawing.Point(12, 418);
+            this.PowerButton.Name = "PowerButton";
+            this.PowerButton.Size = new System.Drawing.Size(46, 23);
+            this.PowerButton.TabIndex = 0;
+            this.PowerButton.Text = "Power";
+            this.PowerButton.UseVisualStyleBackColor = true;
+            this.PowerButton.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BackColor = System.Drawing.Color.Black;
+            this.label1.Font = new System.Drawing.Font("Press Start 2P", 13.25F, System.Drawing.FontStyle.Bold);
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(133, 341);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(27, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = ">";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // Dialogue
+            // 
+            this.Dialogue.AutoSize = true;
+            this.Dialogue.Font = new System.Drawing.Font("Atari Classic Chunky", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Dialogue.Location = new System.Drawing.Point(125, 28);
+            this.Dialogue.Name = "Dialogue";
+            this.Dialogue.Size = new System.Drawing.Size(0, 19);
+            this.Dialogue.TabIndex = 3;
+            this.Dialogue.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // TextTimer
+            // 
+            this.TextTimer.Tick += new System.EventHandler(this.TextTimer_Tick);
+            // 
+            // UserInput
+            // 
+            this.UserInput.BackColor = System.Drawing.SystemColors.MenuText;
+            this.UserInput.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.UserInput.Font = new System.Drawing.Font("Atari Classic Chunky", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.UserInput.ForeColor = System.Drawing.SystemColors.Menu;
+            this.UserInput.Location = new System.Drawing.Point(161, 340);
+            this.UserInput.Name = "UserInput";
+            this.UserInput.Size = new System.Drawing.Size(572, 19);
+            this.UserInput.TabIndex = 4;
+            this.UserInput.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.SystemColors.MenuText;
+            this.panel1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.None;
+            this.panel1.CausesValidation = false;
+            this.panel1.Cursor = System.Windows.Forms.Cursors.Arrow;
+            this.panel1.Enabled = false;
+            this.panel1.Location = new System.Drawing.Point(129, 337);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(604, 25);
+            this.panel1.TabIndex = 5;
+            // 
+            // CursorTimer
+            // 
+            this.CursorTimer.Interval = 400;
+            this.CursorTimer.Tick += new System.EventHandler(this.CursorTimer_Tick);
+            // 
+            // Weatherman
+            // 
+            this.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.ClientSize = new System.Drawing.Size(821, 453);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.Dialogue);
+            this.Controls.Add(this.PowerButton);
+            this.Controls.Add(this.UserInput);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Name = "Weatherman";
+            this.Text = "Weatherman";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button PowerButton;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label Dialogue;
+        private System.Windows.Forms.Timer TextTimer;
+        private TextBox UserInput;
+        private Panel panel1;
+        private Timer CursorTimer;
+    }
+}
+
---
+++ Weatherman/Form1.cs
@@ -0,0 +1,144 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Net;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Weatherman
+{
+    public partial class Weatherman : Form
+    {
+        public Weatherman()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            // Include splash screen perhaps?
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void UserInput_Enter(object sender, EventArgs e)
+        {
+            this.Enabled = false;
+            this.Enabled = true;
+        }
+
+        /// <summary>
+        /// These variables are used to determine what to write using the 'typewriter' method
+        /// </summary>
+
+        private static int _timerIndex = 0;
+        private static string _WelcomeText = "Welcome to Weatherman API";
+
+        /// <summary>
+        /// Nothing out of the ordinary;
+        /// after a 100 ticks this will write out a new letter form the string which contains the message to show.
+        /// This generates a "typewriter" effect
+        /// When the method has iterated through all the letters in the string it will remove the underscore (cursor)
+        /// and place it on the line under.
+        /// </summary>
+
+        private void TextTimer_Tick(object sender, EventArgs e)
+        {
+            Dialogue.Text = _WelcomeText.Substring(0, _timerIndex) + "_";  //Substring is a part of Type_Text String that we declared at the start
+            _timerIndex++;  //Doing a post fix
+            if (_timerIndex == _WelcomeText.Length + 1)
+            {
+                TextTimer.Enabled = false;
+                Dialogue.Text = Dialogue.Text.Replace("_", "\n_");
+
+            }
+        }
+
+        /// <summary>
+        /// This first initiates the timer which controls the tyepwriter effect of the main game dialogue.
+        /// After that it starts the timer which creates a cursor in the input area.
+        /// </summary>
+
+        private void button1_Click(object sender, EventArgs e)  // Power button
+        {
+            // Main dialogue timer
+            _timerIndex = 0; // This ensures that the method will restart writing the content
+
+            if (TextTimer.Enabled || Dialogue.Text.Contains(_WelcomeText))
+            {
+                TextTimer.Enabled = false;  
+            }
+            else
+            {
+                TextTimer.Enabled = true;
+            }
+
+            Dialogue.Text = "";
+
+            // Input timer
+            CursorTimer.Enabled = true;
+            UserInput.Focus();
+            HideCaret(UserInput.Handle);
+        }
+
+        private void textBox1_TextChanged_1(object sender, EventArgs e)
+        {
+            if (UserInput.Text.Length >= 1 && Convert.ToString(UserInput.Text[0]) != "_")
+            {
+                UserInput.Text = UserInput.Text.Replace("_", "");
+                CursorTimer.Enabled = false;
+            }
+            else
+            {
+                CursorTimer.Enabled = true;
+            }
+        }
+
+        private bool IsCursor = false;
+
+        /// <summary>
+        /// This contains the logic to make the cursor blink in the input field
+        /// </summary>
+
+        private void CursorTimer_Tick(object sender, EventArgs e)
+        {
+            if (!IsCursor)
+            {
+                UserInput.Text += "_";
+                IsCursor = true;
+            }
+            else
+            {
+                try
+                {
+                    UserInput.Text = UserInput.Text.Remove(UserInput.Text.Length - 1);
+                    IsCursor = false;
+                }
+                catch (ArgumentOutOfRangeException ex)
+                {
+                    IsCursor = false;
+                    Console.WriteLine(ex.Message); // Debugging
+                }
+            }
+        }
+    }
+}
